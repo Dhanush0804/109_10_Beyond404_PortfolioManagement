fc08146ae61a8a9f1d79227d920643c6c531177a
Abhinav Pandey <abhinavpandeyap22@gmail.com>
2026-07-31 09:30:11 +0000

    initialize project

---
+++ backend/app/src/test/java/com/Beyond404/Portfolio/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Beyond404.Portfolio.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
